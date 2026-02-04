f11c7d7f458e7bede53eee06f6351075812824fa
revanth <brevanthrk@gmail.com>
2026-02-03 19:26:36 -0600

    Fix for OPENAI

--- story_generator.py
+++ story_generator.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI()
 
 def elevate_caption(caption: str) -> str:
     prompt = f"""
